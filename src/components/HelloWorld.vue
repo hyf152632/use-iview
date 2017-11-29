847e56932d671686a2edc768d14936deac7c281e
huo yongfei <773459418@qq.com>
2017-11-29 22:42:00 +0800

    :art: add format

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,66 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Essential Links </h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+      </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
     </ul>
     <h2>this is a lab project.</h2>
     <Button type="primary">Primary</Button>
-    <Button type="info">Info</Button><Icon type="checkmark" />
+    <Button type="info">Info</Button>
+    <Icon type="checkmark" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  data () {
+  name: 'HelloWorld',
+  data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: 'Welcome to Your Vue.js App'
     }
   },
-  created () {
+  created() {
     console.log(this.$_.defaults({ a: 1, b: 2 }, { a: 2 }))
+  },
+  mounted() {
+    console.log('yes i')
+    // this is a commit
+
+    let noSpace = { foo: 'bar' }
+    let noSpaceArr = [1, 2]
+    console.log(noSpaceArr)
+    console.log(noSpace)
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p></p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello'
+    }
+  }
+}
+</script>
+<style scoped>
+.main {
+  color: #fff;
+}
+</style>
+
